98ff4f681ec0bc22b74fc068188348f596afe87a
Totemi1324 <nemestomi2@gmail.com>
2022-10-15 23:16:55 +0200

    Add categories screen layout and data structs

---
+++ lib/data/categories_data.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import '../models/category.dart' as category_model;
+
+const List<category_model.Category> categories = [
+  category_model.Category(
+    id: '1',
+    title: 'Italian',
+    backgroundColor: Colors.purple,
+  ),
+  category_model.Category(
+    id: '2',
+    title: 'Quick & Easy',
+    backgroundColor: Colors.red,
+  ),
+  category_model.Category(
+    id: '3',
+    title: 'Fast Food',
+    backgroundColor: Colors.orange,
+  ),
+  category_model.Category(
+    id: '4',
+    title: 'German',
+    backgroundColor: Colors.amber,
+  ),
+  category_model.Category(
+    id: '5',
+    title: 'Desserts',
+    backgroundColor: Colors.cyan,
+  ),
+  category_model.Category(
+    id: '6',
+    title: 'Breakfast',
+    backgroundColor: Colors.brown,
+  ),
+  category_model.Category(
+    id: '7',
+    title: 'Vegetarian',
+    backgroundColor: Colors.green,
+  ),
+  category_model.Category(
+    id: '8',
+    title: 'Asian',
+    backgroundColor: Colors.lightGreen,
+  ),
+  category_model.Category(
+    id: '9',
+    title: 'French',
+    backgroundColor: Colors.pink,
+  ),
+  category_model.Category(
+    id: '10',
+    title: 'Salads',
+    backgroundColor: Colors.teal,
+  ),
+];
