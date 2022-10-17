fb5eda07b48411a42e6156f8fe9b24740d090922
Tamas Nemes <nemestomi2@gmail.com>
2022-10-17 12:04:08 +0200

    Style text and color themes

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,27 @@ class MyApp extends StatelessWidget {
       title: 'Virtual Cookbook',
       theme: ThemeData(
         primarySwatch: Colors.deepOrange,
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.deepOrange,
+          accentColor: Colors.teal,
+        ),
+        canvasColor: const Color.fromARGB(255, 255, 237, 229),
+        fontFamily: "Raleway",
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(
+            color: Color.fromARGB(255, 20, 51, 51),
+          ),
+          titleMedium: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        appBarTheme: const AppBarTheme(
+          titleTextStyle: TextStyle(
+            fontFamily: "RobotoCondensed",
+            fontSize: 24,
+          ),
+        ),
       ),
       home: const ScreenCategories(),
     );
